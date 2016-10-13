8f046a757218dd7e44c4eab35b4fa3cbfe873905
sksnitesh <sksnitesh@gmail.com>
2016-10-13 12:56:39 +0530

    latest code

---
+++ src/main/java/com/product/dto/response/MessageResponseUser.java
@@ -0,0 +1,11 @@
+package com.product.dto.response;
+
+public class MessageResponseUser {
+	private String id;
+	private String user_from;
+	private String user_to;
+	private String date;
+	private String message;
+	private String image_url;
+
+}
